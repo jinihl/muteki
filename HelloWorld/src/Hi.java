ada3f2497ff5ffe4850dfbb5a1af969e322af69d
jinihl <jinihl@gmail.com>
2016-10-14 17:06:03 +0900

    Update Hi.java

--- HelloWorld/src/Hi.java
+++ HelloWorld/src/Hi.java
@@ -1,8 +1,6 @@
 
 public class Hi {
 	public static void main(String arg[]){
-// 		System.out.println("웋하하하");
-		System.out.println("~~~~");
-// 		System.out.println("뭐");
+		System.out.println("웋하하하");
 	}
 }
